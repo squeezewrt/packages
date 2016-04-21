d4a5b162f411207ec5d881831ffbe580179d075b
Andrew Kazakov <squeezewrt.ak@gmail.com>
2016-04-22 00:17:03 +0300

    Work in progress

--- sound/luci-app-squeezelite/files/controller/squeezelite-controller.lua
+++ sound/luci-app-squeezelite/files/controller/squeezelite-controller.lua
@@ -1,7 +1,7 @@
-module("luci.controller.squeezelite", package.seeall)
+module("luci.controller.squeezelite-controller", package.seeall)
 
 function index()
 	entry({"admin", "services", "squeezelite"}, firstchild(), "Squeezelite", 60)
-	entry({"admin", "services", "squeezelite", "config"}, cbi("squeezelite"), "Configure", 1)
-	entry({"admin", "services", "squeezelite", "info"}, template("squeezelite"), "Info", 2)
+	entry({"admin", "services", "squeezelite", "config"}, cbi("squeezelite-cbi"), "Configure", 1)
+	entry({"admin", "services", "squeezelite", "info"}, template("squeezelite-view"), "Info", 2)
 end
